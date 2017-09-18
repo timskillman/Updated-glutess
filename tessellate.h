75b4e45716acc499b1bcdcf120d0137665548941
timskillman <timskillman@gmail.com>
2017-09-18 14:54:59 +0100

    Update tessellate.h

--- tessellate.h
+++ tessellate.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <vector>
 
-extern void tessellate(std::vector<std::vector<float>> &contours_in, std::vector<float> &tris_out;
+extern void tessellate(std::vector<std::vector<float>> &contours_in, std::vector<float> &tris_out);
