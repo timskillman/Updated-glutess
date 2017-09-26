571cc81754f97e5fd9a0d4225a497ba9da1df7a9
timskillman <timskillman@gmail.com>
2017-09-26 15:35:18 +0100

    Update tessellate.h
    Now returns the boundary for correct winding rules (very useful for 3D extrusion)

--- tessellate.h
+++ tessellate.h
@@ -1,4 +1,6 @@
 #pragma once
 #include <vector>
 
-extern void tessellate(std::vector<std::vector<float>> &contours_in, std::vector<float> &tris_out);
+extern void tessellate(std::vector<std::vector<float>> *contours_in, std::vector<float> *tris_out, 
+                       std::vector<std::vector<float>> *edges, bool getBounds = false, float z = -0.9f);
+
